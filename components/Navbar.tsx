04766df2651912aa3618392081ea94780375380d
Preet Mishra <ipreetmishra@gmail.com>
2021-12-21 17:56:37 +0530

    Improve NavBar design.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -30,20 +30,20 @@ const Navbar: FunctionComponent = () => {
 
   return (
     <nav className="sticky top-0 z-50 flex justify-center w-full h-16 font-serif bg-white border-b border-gray-100/90 md:h-20">
-      <div className="flex items-center justify-between w-full px-6 space-x-4 text-gray-700 md:space-x-8 md:max-w-6xl md:px-8">
+      <div className="flex items-center justify-between w-full px-6 space-x-4 font-medium tracking-tight text-gray-900/50 md:space-x-8 md:max-w-6xl md:px-8">
         <h1 className="flex-none">
           <Link href={ROUTE_HOME}>
             <a title={AUTHOR_FULL_NAME}>{AUTHOR_FULL_NAME}</a>
           </Link>
         </h1>
-        <ul className="flex flex-wrap text-gray-700 text-opacity-50 gap-x-4">
+        <ul className="flex flex-wrap gap-x-4">
           {NAV_LINKS.map(({ path, label }, index) => (
             <li key={index}>
               <Link href={path}>
                 <a
                   className={
-                    "hover:text-gray-700" +
-                    (isActive(path) ? " text-gray-700" : "")
+                    "hover:text-gray-600" +
+                    (isActive(path) ? " text-gray-600" : "")
                   }
                   title={label}
                 >
