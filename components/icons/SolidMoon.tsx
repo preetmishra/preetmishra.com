4dca5a975c4fab0f579adcbd3613742a0d5385b0
Preet Mishra <ipreetmishra@gmail.com>
2022-05-07 15:40:02 +0530

    Add animation for theme toggle buttons.

--- components/icons/SolidMoon.tsx
+++ components/icons/SolidMoon.tsx
@@ -1,18 +1,37 @@
 import React, { FunctionComponent } from "react";
+import { motion } from "framer-motion";
 
 type Props = {
   className: string;
 };
 
 const SolidMoon: FunctionComponent<Props> = ({ className }) => (
-  <svg
+  <motion.svg
     xmlns="http://www.w3.org/2000/svg"
     className={className}
     viewBox="0 0 20 20"
     fill="currentColor"
+    initial={{
+      scale: 0.5,
+      rotate: 90,
+    }}
+    animate={{
+      scale: 1,
+      rotate: 0,
+    }}
+    whileHover={{
+      scale: 1.1,
+    }}
+    whileTap={{
+      scale: 0.5,
+    }}
+    transition={{
+      type: "spring",
+      bounce: 0.25,
+    }}
   >
     <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
-  </svg>
+  </motion.svg>
 );
 
 export default SolidMoon;
